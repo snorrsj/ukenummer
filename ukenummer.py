b40e12e95e4272a6b6d3d14b204f6804c24ec08f
Snorre Sjaatil <snorresjaatil@outlook.com>
2023-01-23 12:48:31 +0100

    Added current date

--- ukenummer.py
+++ ukenummer.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 
 URL = 'https://ukenr.no/'
 page = requests.get(URL)
@@ -7,4 +8,12 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="ugenr")
 
+day = datetime.datetime.today().weekday()
+days = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"]
+now = datetime.datetime.now()
+month = ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"]
+
+
 print(f'Det er {results.text}')
+print(f'{days[day]}, {now.strftime("%d")}. {month[now.month]} {now.strftime("%Y")}')
+
